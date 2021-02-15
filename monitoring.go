5d5084ee1cda6a54e6d88a5ba3993ee4c57689af
AntonioGSC <antoniogabrielagsc@gmail.com>
2021-02-15 20:36:18 -0300

    monitoring slack site

--- monitoring.go
+++ monitoring.go
@@ -2,25 +2,30 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
-	introduction()
-	menu()
-	numberComand := getComand()
 
-	switch numberComand {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
-		os.Exit(-1)
+	for {
+		introduction()
+		menu()
+		numberComand := getComand()
+
+		switch numberComand {
+		case 1:
+			initMonitoring()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
+
 	}
 }
 
@@ -42,4 +47,16 @@ func getComand() int {
 	var comand int
 	fmt.Scan(&comand)
 	return comand
+}
+
+func initMonitoring() {
+	fmt.Println("Monitorando...")
+	site := "https://slack.com/intl/pt-br/"
+	response, _ := http.Get(site)
+	// fmt.Println(response)
+	if response.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "esta com problemas. Status Code:", response.StatusCode)
+	}
 }
