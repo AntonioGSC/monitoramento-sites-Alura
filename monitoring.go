be4fdd49f0563a849a050e3658d9e0e01594def4
AntonioGSC <antoniogabrielagsc@gmail.com>
2021-02-14 21:52:00 -0300

    functions and menu

--- monitoring.go
+++ monitoring.go
@@ -1,20 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	introduction()
+	menu()
+	numberComand := getComand()
+
+	switch numberComand {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa...")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
+	}
+}
+
+func introduction() {
 	name := "Tony"
 	version := 1.1
 
 	fmt.Println("Olá", name)
 	fmt.Println("Bem-vindo ao monitoramento, esse programa está na versão", version)
+}
 
+func menu() {
 	fmt.Println("1 - Iniciar Monitoramento")
 	fmt.Println("2 - Exibir Logs")
 	fmt.Println("0 - Sair do programa")
+}
 
-	var number int
-	//fmt.Scanf("%d", &number)
-	fmt.Scan(&number)
-	fmt.Println("Variavel a seguir", number)
+func getComand() int {
+	var comand int
+	fmt.Scan(&comand)
+	return comand
 }
