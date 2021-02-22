9a3f09f89c47bac2580ba7a26ef8a8f3d98c0c3c
AntonioGSC <antoniogabrielagsc@gmail.com>
2021-02-22 20:44:44 -0300

    Add txt and FileRead

--- monitoring.go
+++ monitoring.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 )
+
+const monitoring = 3
+const timeDelay = 2
 
 func main() {
 
@@ -46,17 +53,58 @@ func menu() {
 func getComand() int {
 	var comand int
 	fmt.Scan(&comand)
+	fmt.Println("")
 	return comand
 }
 
 func initMonitoring() {
 	fmt.Println("Monitorando...")
-	site := "https://slack.com/intl/pt-br/"
-	response, _ := http.Get(site)
-	// fmt.Println(response)
+	// sites := []string{"https://slack.com/intl/pt-br/", "https://github.com/", "https://hub.docker.com/", "https://www.linkedin.com/"}
+	sites := getArchiveSites()
+	for i := 0; i < monitoring; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			siteTest(site)
+		}
+		time.Sleep(timeDelay * time.Second)
+		fmt.Println("")
+	}
+}
+
+func siteTest(site string) {
+	response, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro", nil)
+	}
+
 	if response.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
 	} else {
 		fmt.Println("Site:", site, "esta com problemas. Status Code:", response.StatusCode)
 	}
+}
+
+func getArchiveSites() []string {
+	// archive, err := ioutil.ReadFile("sites.txt")
+	var sites []string
+
+	archive, err := os.Open("sites.txt")
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	reader := bufio.NewReader(archive)
+	for {
+		line, err := reader.ReadString('\n')
+		line = strings.TrimSpace(line)
+		sites = append(sites, line)
+		if err == io.EOF {
+			break
+		}
+	}
+
+	archive.Close()
+
+	return sites
 }
